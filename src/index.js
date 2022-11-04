269e1e656d88b68bb25e1ea40829b0a1a8d72a58
ernest korir <ernestbore3@gmail.com>
2022-11-04 11:56:33 +0300

    Interactive To Do completed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './index.css';
 
 import inject from './modules/inject.js';
 import { getAddedTodos, form } from './modules/newtodo.js';
+import clearCompleted from './modules/clearCompleted.js'
 
 inject();
-
 form.addEventListener('submit', getAddedTodos);
+clearCompleted();
