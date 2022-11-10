2c75120b840cba63aae3aaeb556a49387cef9834
ernest korir <ernestbore3@gmail.com>
2022-11-10 20:54:53 +0300

    modified the modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import './index.css';
-
 import inject from './modules/inject.js';
 import { getAddedTodos, form } from './modules/newtodo.js';
 import clearCompleted from './modules/clearCompleted.js';
+import deleteTask from './modules/deleteTask.js';
+
+const clearCompletedButton = document.getElementById('clear-completed');
 
 inject();
 form.addEventListener('submit', getAddedTodos);
 clearCompleted();
+
+document.addEventListener('click', (e) => {
+    if (e.target.className === 'fa fa-times') {
+      deleteTask(e.target.id);
+    }
+  });
+  
+  clearCompletedButton.addEventListener('click', clearCompleted);
