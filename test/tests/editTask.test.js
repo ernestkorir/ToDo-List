67e8b3752654d19f4476f92c699af03504e13f37
ernest korir <ernestbore3@gmail.com>
2022-11-10 20:56:11 +0300

    Clear and item status test

---
+++ test/tests/editTask.test.js
@@ -0,0 +1,7 @@
+import createMockDocument from '../mocks/documentMock.js';
+import { getAddedTodos } from '../../src/modules/newtodo.js';
+import { todo } from '../../src/modules/displayList.js';
+
+function addInputDescription(value, input) {
+  input.value = value;
+}
